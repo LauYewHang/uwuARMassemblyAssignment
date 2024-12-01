f4435e602d8130127c0a4f1861f182681d06f9af
Sunshine <yewhang2004@gmail.com>
2024-12-02 03:56:40 +0800

    complete the merge function for uwuMergeSort.s, I am the best uwu

--- uwuMergeSort.s
+++ uwuMergeSort.s
@@ -8,10 +8,20 @@ uwuRight RN R2 ; reserve R2 for variable "right"
 
 uwuReadFrom RN R3 ; address of array to read from uwu
 uwuWriteTo RN R4 ; address of array to write to uwu
-uwuIndexFrom RN R5 ; read from which index (inclusive) uwu
-uwuIndexTo RN R6 ; read to which index (exclusive) uwu
+uwuStartInt RN R5 ; starting int uwu (inclusive)
+uwuEndInt RN R6 ; end int uwu (exclusive)
 
-uwuHold RN R12 ; use to hold random value uwu
+uwuLeftS RN R7 ; keep track of size of left array in merge process
+uwuRightS RN R8 ; keep track of size of right array in merge process
+uwuI RN R9 ; keep track of indexing of left array in merge
+uwuJ RN R10 ; keep track of indexting of right array in merge
+
+uwuLeftArrayR RN R3 ; register to keep track of left array's address
+uwuRightArrayR RN R4 ; register to keep track of right array's address
+uwuSortedArrayR RN R5 ; register to keep track of sorted array's address
+
+uwuHold RN R11 ; use to hold random value uwu
+uwuHold2 RN R12 ; use to hold random value2 uwu
 
 uwuArraySize EQU 10 ; constant for array size uwu
 uwuWord EQU 4 ; constant for word size uwu
@@ -25,26 +35,24 @@ uwuInitialize
 	
 	LDR uwuReadFrom, =uwuUnsortedArray ; read from unsorted array uwu
 	LDR uwuWriteTo, =uwuSortedArray ; write to sorted array
-	MOV uwuIndexFrom, #0 ; read from first index
-	MOV uwuIndexTo, #uwuArraySize ; until the last
+	MOV uwuStartInt, #0 ; read from first index
+	MOV uwuEndInt, #uwuArraySize ; until the last
 	BL uwuReadArray ; call the read array function
 	
 	MOV uwuHold, PC ; store current PC
 	ADD uwuHold, #8 ; offset 12 bytes (3 instructions) since we want to skip PUSH and uwuMergeSort
-	PUSH {uwuHold} ; store the PC so next time the last function POP it jumps back here uwu
+	PUSH {uwuLeft, uwuMid, uwuRight, uwuHold} ; store the PC so next time the last function POP it jumps back here uwu
 	B uwuMergeSort ; call mergeSort function
 	B uwuStop ; uwu stop
 	
-uwuReadArray
-	MOV uwuHold, #uwuWord ; hold 4 bytes offset
-	MUL uwuHold, uwuIndexFrom, uwuHold ; get the offset of index (in bytes form)
-	
-	LDR uwuHold, [uwuReadFrom, uwuHold] ; load the value from array A (address of A + offset, where the reading should start)
+uwuReadArray	
+	LDR uwuHold, [uwuReadFrom] ; load the value from array A
 	STR uwuHold, [uwuWriteTo] ; store the value to array B
 	
+	ADD uwuReadFrom, #uwuWord
 	ADD uwuWriteTo, #uwuWord ; add 4 bytes offset to array B (get address to write next value)
-	ADD uwuIndexFrom, #1 ; increment iteration by 1
-	CMP uwuIndexFrom, uwuIndexTo ; compare the condition
+	ADD uwuStartInt, #1 ; increment iteration by 1
+	CMP uwuStartInt, uwuEndInt ; compare the condition
 	BLT uwuReadArray ; repeat until indexFrom >= indexTo
 	BX LR ; branch back after completing the loop
 	
@@ -80,30 +88,85 @@ uwuMergeSort ; uwuMergeSort function with parameters "left" & "right", uwuMergeS
 	B uwuMerge ; merge the two array together uwu
 
 uwuMerge
+	; initialize left and right array size
+	SUB uwuLeftS, uwuMid, uwuLeft
+	ADD uwuLeftS, #1 ; left array size = (mid-left) + 1
+	SUB uwuRightS, uwuRight, uwuMid ; right array size = (right-mid)
+
 	; store to left array
 	LDR uwuReadFrom, =uwuSortedArray
+	MOV uwuHold, #uwuWord
+	MUL uwuHold, uwuLeft, uwuHold
+	ADD uwuReadFrom, uwuHold
 	LDR uwuWriteTo, =uwuLeftArray
-	MOV uwuIndexFrom, uwuLeft
-	MOV uwuIndexTo, uwuMid
+	MOV uwuStartInt, #0
+	MOV uwuEndInt, uwuLeftS
 	BL uwuReadArray
 	
 	; store to right array
 	LDR uwuReadFrom, =uwuSortedArray
+	MOV uwuHold, #uwuWord
+	MUL uwuHold, uwuMid, uwuHold
+	ADD uwuHold, #uwuWord
+	ADD uwuReadFrom, uwuHold
 	LDR uwuWriteTo, =uwuRightArray
-	MOV uwuIndexFrom, uwuMid
-	MOV uwuIndexTo, uwuRight
-	ADD uwuIndexTo, #1
+	MOV uwuStartInt, #0
+	MOV uwuEndInt, uwuRightS
 	BL uwuReadArray
 	
+	LDR uwuLeftArrayR, =uwuLeftArray
+	LDR uwuRightArrayR, =uwuRightArray
+	LDR uwuSortedArrayR, =uwuSortedArray
+	MOV uwuHold, #uwuWord
+	MUL uwuHold, uwuLeft, uwuHold
+	ADD uwuSortedArrayR, uwuHold
+	MOV uwuI, #0
+	MOV uwuJ, #0
 	BL compare
+	BL uwuStoreLeft
+	BL uwuStoreRight
+	POP {uwuLeft, uwuMid, uwuRight, PC}
 	
 compare
+	CMP uwuI, uwuLeftS
+	CMPLT uwuJ, uwuRightS
+	BXGE LR
+	LDR uwuHold, [uwuLeftArrayR]
+	LDR uwuHold2, [uwuRightArrayR]
+	CMP uwuHold, uwuHold2
+	STRLT uwuHold, [uwuSortedArrayR]
+	ADDLT uwuLeftArrayR, #uwuWord
+	ADDLT uwuI, #1
+	STRGE uwuHold2, [uwuSortedArrayR]
+	ADDGE uwuRightArrayR, #uwuWord
+	ADDGE uwuJ, #1
+	ADD uwuSortedArrayR, #uwuWord
+	B compare
+
+uwuStoreLeft
+	CMP uwuI, uwuLeftS
+	BXGE LR
+	LDR uwuHold, [uwuLeftArrayR]
+	STR uwuHold, [uwuSortedArrayR]
+	ADD uwuLeftArrayR, #uwuWord
+	ADD uwuI, #1
+	ADD uwuSortedArrayR, #uwuWord
+	B uwuStoreLeft
 	
-	
+uwuStoreRight
+	CMP uwuJ, uwuRightS
+	BXGE LR
+	LDR uwuHold2, [uwuRightArrayR]
+	STR uwuHold2, [uwuSortedArrayR]
+	ADD uwuRightArrayR, #uwuWord
+	ADD uwuJ, #1
+	ADD uwuSortedArrayR, #uwuWord
+	B uwuStoreRight
+
 ; uwu end
 uwuStop B uwuStop	
 	
-uwuUnsortedArray DCD 8, 29, 50, 81, 4, 23, 24, 30, 1, 7 ; original unsorted array uwu	
+uwuUnsortedArray DCD 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ; original unsorted array uwu	
 	
 	AREA uwuMemory, DATA, READWRITE
 uwuStackMemory SPACE 4096 ; used for storing stack value (PUSH and POP) uwu
