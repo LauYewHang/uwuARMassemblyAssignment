ddc5cb13d8296c08914ea229bd43c5a4e592264c
Sunshine <yewhang2004@gmail.com>
2024-12-02 00:20:37 +0800

    add uwuMergeSort.s uwu

---
+++ uwuMergeSort.s
@@ -0,0 +1,17 @@
+	AREA uwuCode, CODE, READONLY
+	ENTRY
+	
+	
+	
+mergeSort
+
+merge
+	
+uwuUnsortedArray DCD 	
+	
+	AREA uwuMemory, DATA, READONLY
+uwuStackMemory SPACE 4096 ; used for storing stack value (PUSH and POP) uwu
+uwuLeftArray SPACE 40 ; used to store left side array during merge process uwu
+uwuRightArray SPACE 40 ; used to store right side array during merge process uwu
+uwuSortedArray SPACE 40 ; used to store the sorted array uwu
+	END
