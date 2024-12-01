dbd2adc0c754f47ff4cb34b25d57aeb64c5a313e
Sunshine <yewhang2004@gmail.com>
2024-12-02 04:37:33 +0800

    add comment to uwuMergeSort.s uwu

--- uwuMergeSort.s
+++ uwuMergeSort.s
@@ -9,7 +9,7 @@ uwuRight RN R2 ; reserve R2 for variable "right"
 uwuReadFrom RN R3 ; address of array to read from uwu
 uwuWriteTo RN R4 ; address of array to write to uwu
 uwuStartInt RN R5 ; starting int uwu (inclusive)
-uwuEndInt RN R6 ; end int uwu (exclusive)
+uwuEndInt RN R6 ; end int uwu (exclusive), these two basically means: for (int start; start < end)
 
 uwuLeftS RN R7 ; keep track of size of left array in merge process
 uwuRightS RN R8 ; keep track of size of right array in merge process
@@ -42,18 +42,18 @@ uwuInitialize
 	MOV uwuHold, PC ; store current PC
 	ADD uwuHold, #8 ; offset 12 bytes (3 instructions) since we want to skip PUSH and uwuMergeSort
 	PUSH {uwuLeft, uwuMid, uwuRight, uwuHold} ; store the PC so next time the last function POP it jumps back here uwu
-	B uwuMergeSort ; call mergeSort function
+	B uwuMergeSort ; call mergeSort function uwu
 	B uwuStop ; uwu stop
 	
 uwuReadArray	
 	LDR uwuHold, [uwuReadFrom] ; load the value from array A
 	STR uwuHold, [uwuWriteTo] ; store the value to array B
 	
-	ADD uwuReadFrom, #uwuWord
+	ADD uwuReadFrom, #uwuWord ; increment the read address by 4 bytes (points to next element)
 	ADD uwuWriteTo, #uwuWord ; add 4 bytes offset to array B (get address to write next value)
 	ADD uwuStartInt, #1 ; increment iteration by 1
 	CMP uwuStartInt, uwuEndInt ; compare the condition
-	BLT uwuReadArray ; repeat until indexFrom >= indexTo
+	BLT uwuReadArray ; repeat until startInt >= endInt uwu
 	BX LR ; branch back after completing the loop
 	
 uwuMergeSort ; uwuMergeSort function with parameters "left" & "right", uwuMergeSort(left, right)
@@ -68,7 +68,7 @@ uwuMergeSort ; uwuMergeSort function with parameters "left" & "right", uwuMergeS
 	MOV uwuHold, PC
 	ADD uwuHold, #16 ; current PC with 16 bytes offset since we want to skip 2 PUSH, 1 MOV, and first uwuMergeSort
 	
-	PUSH {uwuHold} ; save the PC address uwu
+	PUSH {uwuHold} ; save the PC address uwu (always PC first, values later, because during POP it read from top to bottom, and R0 has higher priority than R12)
 	PUSH {uwuLeft, uwuMid, uwuRight} ; save the current left mid right value uwu
 	MOV uwuRight, uwuMid ; right = mid
 	B uwuMergeSort ; call uwuMergeSort with (left, mid)
@@ -78,8 +78,8 @@ uwuMergeSort ; uwuMergeSort function with parameters "left" & "right", uwuMergeS
 	MOV uwuHold, PC
 	ADD uwuHold, #20 ; current PC with 20 bytes offset since we want to skip 2 PUSH, 1 MOV, 1 ADD, 1 uwuMergeSort
 	
-	PUSH {uwuHold}
-	PUSH {uwuLeft, uwuMid, uwuRight}
+	PUSH {uwuHold} ; save the PC address uwu
+	PUSH {uwuLeft, uwuMid, uwuRight} ; save the values uwu
 	
 	MOV uwuLeft, uwuMid
 	ADD uwuLeft, #1 ; left = mid + 1
@@ -94,74 +94,74 @@ uwuMerge
 	SUB uwuRightS, uwuRight, uwuMid ; right array size = (right-mid)
 
 	; store to left array
-	LDR uwuReadFrom, =uwuSortedArray
-	MOV uwuHold, #uwuWord
-	MUL uwuHold, uwuLeft, uwuHold
-	ADD uwuReadFrom, uwuHold
-	LDR uwuWriteTo, =uwuLeftArray
-	MOV uwuStartInt, #0
-	MOV uwuEndInt, uwuLeftS
-	BL uwuReadArray
+	LDR uwuReadFrom, =uwuSortedArray ; read from sorted array uwu
+	MOV uwuHold, #uwuWord ; save offset 4 to hold
+	MUL uwuHold, uwuLeft, uwuHold ; multiply the offset by left's value (since when reading from the sorted array, it is supposed to start from sortedArray[left], for left temporary array)
+	ADD uwuReadFrom, uwuHold ; add the offset to uwuReadFrom
+	LDR uwuWriteTo, =uwuLeftArray ; write to the left temporary array uwu
+	MOV uwuStartInt, #0 ; start from 0
+	MOV uwuEndInt, uwuLeftS ; end when it is >= left array size uwu
+	BL uwuReadArray ; uwu go read the array now
 	
 	; store to right array
-	LDR uwuReadFrom, =uwuSortedArray
-	MOV uwuHold, #uwuWord
-	MUL uwuHold, uwuMid, uwuHold
-	ADD uwuHold, #uwuWord
-	ADD uwuReadFrom, uwuHold
-	LDR uwuWriteTo, =uwuRightArray
-	MOV uwuStartInt, #0
-	MOV uwuEndInt, uwuRightS
-	BL uwuReadArray
+	LDR uwuReadFrom, =uwuSortedArray ; read from sorted array uwu
+	MOV uwuHold, #uwuWord ; save offset 4 to hold
+	MUL uwuHold, uwuMid, uwuHold ; multiply the offset by mid's value
+	ADD uwuHold, #uwuWord ; add the offset by 4 (mid + 1)
+	ADD uwuReadFrom, uwuHold ; add the offset to the uwuReadFrom
+	LDR uwuWriteTo, =uwuRightArray ; write to the right temporary array
+	MOV uwuStartInt, #0 ; uwu start from 0
+	MOV uwuEndInt, uwuRightS ; end when it is >= right array size uwu
+	BL uwuReadArray ; uwu go read the array now
 	
-	LDR uwuLeftArrayR, =uwuLeftArray
-	LDR uwuRightArrayR, =uwuRightArray
-	LDR uwuSortedArrayR, =uwuSortedArray
-	MOV uwuHold, #uwuWord
-	MUL uwuHold, uwuLeft, uwuHold
+	LDR uwuLeftArrayR, =uwuLeftArray ; load address of left array
+	LDR uwuRightArrayR, =uwuRightArray ; load address of right array
+	LDR uwuSortedArrayR, =uwuSortedArray ; load address of sorted array
+	MOV uwuHold, #uwuWord ; hold 4 as offset
+	MUL uwuHold, uwuLeft, uwuHold ; multiply the offset by 
 	ADD uwuSortedArrayR, uwuHold
-	MOV uwuI, #0
-	MOV uwuJ, #0
-	BL compare
-	BL uwuStoreLeft
-	BL uwuStoreRight
-	POP {uwuLeft, uwuMid, uwuRight, PC}
+	MOV uwuI, #0 ; set I as 0 uwu
+	MOV uwuJ, #0 ; set J as 0 uwu
+	BL compare ; go to compare uwu
+	BL uwuStoreLeft ; store the remaining values in left array uwu
+	BL uwuStoreRight ; store the remaining values in right array uwu
+	POP {uwuLeft, uwuMid, uwuRight, PC} ; we going bakkkk uwu
 	
 compare
-	CMP uwuI, uwuLeftS
-	CMPLT uwuJ, uwuRightS
-	BXGE LR
-	LDR uwuHold, [uwuLeftArrayR]
-	LDR uwuHold2, [uwuRightArrayR]
-	CMP uwuHold, uwuHold2
-	STRLT uwuHold, [uwuSortedArrayR]
-	ADDLT uwuLeftArrayR, #uwuWord
-	ADDLT uwuI, #1
-	STRGE uwuHold2, [uwuSortedArrayR]
-	ADDGE uwuRightArrayR, #uwuWord
-	ADDGE uwuJ, #1
-	ADD uwuSortedArrayR, #uwuWord
-	B compare
+	CMP uwuI, uwuLeftS ; check if I < left array size
+	CMPLT uwuJ, uwuRightS ; check if J < right array size
+	BXGE LR ; if not, return back
+	LDR uwuHold, [uwuLeftArrayR] ; get value from left array
+	LDR uwuHold2, [uwuRightArrayR] ; get value from right array
+	CMP uwuHold, uwuHold2 ; compare
+	STRLT uwuHold, [uwuSortedArrayR] ; store left is less than
+	ADDLT uwuLeftArrayR, #uwuWord ; increment left array address
+	ADDLT uwuI, #1 ; increment for I
+	STRGE uwuHold2, [uwuSortedArrayR] ; store right if hold >= hold2
+	ADDGE uwuRightArrayR, #uwuWord ; increment right array address
+	ADDGE uwuJ, #1 ; increment for J
+	ADD uwuSortedArrayR, #uwuWord ; increment the address of sorted array
+	B compare ; repeat
 
 uwuStoreLeft
-	CMP uwuI, uwuLeftS
-	BXGE LR
-	LDR uwuHold, [uwuLeftArrayR]
-	STR uwuHold, [uwuSortedArrayR]
-	ADD uwuLeftArrayR, #uwuWord
-	ADD uwuI, #1
-	ADD uwuSortedArrayR, #uwuWord
-	B uwuStoreLeft
+	CMP uwuI, uwuLeftS ; compare I and size of left array
+	BXGE LR ; return back if I >= size
+	LDR uwuHold, [uwuLeftArrayR] ; take value from left array
+	STR uwuHold, [uwuSortedArrayR] ; store to sorted array
+	ADD uwuLeftArrayR, #uwuWord ; increment the left array address by 4
+	ADD uwuI, #1 ; increment I by 1
+	ADD uwuSortedArrayR, #uwuWord ; increment the sorted array's address by 4 (points to next value)
+	B uwuStoreLeft ; repeat uwu
 	
 uwuStoreRight
-	CMP uwuJ, uwuRightS
-	BXGE LR
-	LDR uwuHold2, [uwuRightArrayR]
-	STR uwuHold2, [uwuSortedArrayR]
-	ADD uwuRightArrayR, #uwuWord
-	ADD uwuJ, #1
-	ADD uwuSortedArrayR, #uwuWord
-	B uwuStoreRight
+	CMP uwuJ, uwuRightS ; compare J and size of right array
+	BXGE LR ; return back if J >= size
+	LDR uwuHold2, [uwuRightArrayR] ; take value from right array
+	STR uwuHold2, [uwuSortedArrayR] ; store to sorted array
+	ADD uwuRightArrayR, #uwuWord ; increment the right array address by 4
+	ADD uwuJ, #1 ; increment J by 1
+	ADD uwuSortedArrayR, #uwuWord ; increment the sorted array's address by 4 (points to next value)
+	B uwuStoreRight ; repeat uwu
 
 ; uwu end
 uwuStop B uwuStop	
